7fbd8786f4fe062258ea911a90d4416eefcc4550
RADHIKA SHRIVASTAVA <shrivastavradhika89@gmail.com>
2026-02-03 22:59:53 +0530

    Added basic recursion programs of DSA in C++

---
+++ Recursion_programs/sum of n terms_recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumN(int n)
+{
+    if(n == 0)   
+        return 0;
+    return n + sumN(n - 1);  
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Sum = " << sumN(n);
+    return 0;
+}
